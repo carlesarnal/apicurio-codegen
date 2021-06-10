7112197eff1c4e04d73eccd3cd9a80409ccf47ed
Carles Arnal <carlesarnal92@gmail.com>
2021-06-10 11:31:28 +0200

    Add codegen skeleton

--- codegen/src/main/java/io/quarkus/apicurio/codegen/deployment/OpenApiCodeGenProvider.java
+++ codegen/src/main/java/io/quarkus/apicurio/codegen/deployment/OpenApiCodeGenProvider.java
@@ -1,11 +1,21 @@
 package io.quarkus.apicurio.codegen.deployment;
 
+import io.apicurio.hub.api.codegen.OpenApi2JaxRs;
 import io.quarkus.bootstrap.prebuild.CodeGenException;
 import io.quarkus.deployment.CodeGenContext;
 import io.quarkus.deployment.CodeGenProvider;
+import io.quarkus.utilities.OS;
+import org.apache.commons.io.FileUtils;
 import org.jboss.logging.Logger;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.lang.Boolean.TRUE;
 
@@ -36,10 +46,48 @@ public class OpenApiCodeGenProvider implements CodeGenProvider {
             log.info("Skipping " + this.getClass() + " invocation on user's request");
             return false;
         }
+
         Path outDir = context.outDir();
         Path workDir = context.workDir();
-        Path protoDir = context.inputDir();
+        Path inputDir = context.inputDir();
 
-        return false;
+        OpenApi2JaxRs.JaxRsProjectSettings settings = new OpenApi2JaxRs.JaxRsProjectSettings();
+        settings.codeOnly = false;
+        settings.artifactId = "generated-api";
+        settings.groupId = "org.example.api";
+        settings.javaPackage = "org.example.api";
+
+        try {
+            if (Files.isDirectory(inputDir)) {
+                try (Stream<Path> protoFilesPaths = Files.walk(inputDir)) {
+                    String openApiDefs = protoFilesPaths
+                            .filter(Files::isRegularFile)
+                            .map(Path::toString)
+                            .filter(s -> s.endsWith(OPEN_API))
+                            .map(this::escapeWhitespace)
+                            .findFirst()
+                            .orElseThrow(() -> new CodeGenException("Failed to create java project from openapi definition, no definition found"));
+
+                    OpenApi2JaxRs generator = new OpenApi2JaxRs();
+                    generator.setSettings(settings);
+                    generator.setOpenApiDocument(getClass().getClassLoader().getResource(openApiDefs));
+                    ByteArrayOutputStream outputStream = null;
+                    outputStream = generator.generate();
+                    File tempFile = File.createTempFile("api", ".zip");
+                    FileUtils.writeByteArrayToFile(tempFile, outputStream.toByteArray());
+                }
+            }
+            return true;
+        } catch (IOException e) {
+            throw new CodeGenException("Failed to create java project from openapi definition", e);
+        }
+    }
+
+    private String escapeWhitespace(String path) {
+        if (OS.determineOS() == OS.LINUX) {
+            return path.replace(" ", "\\ ");
+        } else {
+            return path;
+        }
     }
 }
