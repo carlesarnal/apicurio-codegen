7ff2c16a528ec20b04bf9cd1269581535e605ebc
Carles Arnal <carlesarnal92@gmail.com>
2021-06-27 19:32:20 +0200

    Improve description

--- codegen/src/test/java/io/quarkus/apicurio/codegen/deployment/wrapper/OpenApiServerGeneratorWrapperTest.java
+++ codegen/src/test/java/io/quarkus/apicurio/codegen/deployment/wrapper/OpenApiServerGeneratorWrapperTest.java
@@ -11,7 +11,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
